fa54d9a567900da64e3af7325ac635d17b00cb9b
Bernardo Barros <bernardo.barros@protonamil.ch>
2024-11-29 16:53:53 -0500

    type definitions with explicit deriving strategies. Added DerivingStrategies language pragma

--- src/Euterpea/IO/Audio/Types.hs
+++ src/Euterpea/IO/Audio/Types.hs
@@ -1,3 +1,5 @@
+-- Added for explicit deriving strategy
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -5,38 +7,80 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module Euterpea.IO.Audio.Types where
+module Euterpea.IO.Audio.Types (
+    -- * Clock types
+    Clock (..),
+    AudRate,
+    CtrRate,
+
+    -- * Error types
+    AudioError (..),
+
+    -- * Frequency
+    Frequency (..),
+    ToFrequency (..),
+
+    -- * Sample rate
+    SampleRate (..),
+
+    -- * Signal types
+    AudSF,
+    CtrSF,
+    Signal,
+    SigFun,
+
+    -- * Audio sample types
+    AudioSample (..),
+    MultiChannel (..),
+    AudioChannel (..),
+
+    -- * Channel configurations
+    Mono,
+    Stereo,
+) where
 
 import Control.Arrow.ArrowP
 import Control.SF.SF
 import Data.Maybe (fromMaybe)
 
--- | The 'Clock' class is used to define types that can provide a sampling rate.
+-- | The 'Clock' class defines types that can provide a sampling rate.
 class Clock p where
-    -- | Returns the sampling rate for the given clock type.
+    -- | Returns the sampling rate in Hz for the given clock type.
     rate :: p -> Double
 
+-- | Audio rate clock type (44.1 kHz)
 data AudRate
+
+-- | Control rate clock type (4.41 kHz)
 data CtrRate
 
 instance Clock AudRate where
-    rate _ = 44100  -- Audio rate (44100 Hz)
+    rate _ = 44100 -- Audio rate (44.1 kHz)
 
 instance Clock CtrRate where
-    rate _ = 4410   -- Control rate (4410 Hz)
+    rate _ = 4410 -- Control rate (4.41 kHz)
 
 -- | Represents various types of audio processing errors
 data AudioError
-    = InvalidFrequency Double -- ^ Frequency outside valid range (20Hz-20kHz)
-    | InvalidSampleRate Double -- ^ Sample rate outside valid range (1Hz-192kHz)
-    | InvalidPan Double        -- ^ Pan outside valid range (-1 to 1)
-    deriving (Show, Eq)
+    = -- | Frequency outside valid range (20Hz-20kHz)
+      InvalidFrequency Double
+    | -- | Sample rate outside valid range (1Hz-192kHz)
+      InvalidSampleRate Double
+    | -- | Pan outside valid range (-1 to 1)
+      InvalidPan Double
+    deriving stock (Show, Eq)
 
-newtype Frequency = Frequency {getFrequency :: Double}
-    deriving (Show, Eq, Ord, Num, Fractional, Floating)
+-- | Represents frequency in Hz
+newtype Frequency = Frequency
+    { getFrequency :: Double
+    -- ^ Get the frequency value in Hz
+    }
+    deriving stock (Show, Eq, Ord)
+    deriving newtype (Num, Fractional, Floating)
 
+-- | Types that can be converted to a frequency value
 class ToFrequency a where
-    -- | Converts a value to a 'Frequency'.
+    -- | Converts a value to a 'Frequency' in Hz
     toFrequency :: a -> Frequency
 
 instance ToFrequency Double where
@@ -45,18 +89,38 @@ instance ToFrequency Double where
 instance ToFrequency Frequency where
     toFrequency = id
 
-newtype SampleRate = SampleRate {getSampleRate :: Double}
-    deriving (Show, Eq, Ord, Num)
+-- | Represents sample rate in Hz
+newtype SampleRate = SampleRate
+    { getSampleRate :: Double
+    -- ^ Get the sample rate value in Hz
+    }
+    deriving stock (Show, Eq, Ord)
+    deriving newtype (Num)
 
+-- | Audio rate signal function
 type AudSF a b = SigFun AudRate a b
+
+-- | Control rate signal function
 type CtrSF a b = SigFun CtrRate a b
+
+-- | Generic signal type
 type Signal clk a b = ArrowP SF clk a b
+
+-- | Signal function type
 type SigFun clk a b = ArrowP SF clk a b
 
+-- | Type class for audio samples that can be mixed and processed
 class AudioSample a where
+    -- | Zero sample value
     zero :: a
+
+    -- | Mix two samples together
     mix :: a -> a -> a
+
+    -- | Convert sample to list of channel values
     collapse :: a -> [Double]
+
+    -- | Get number of channels
     numChans :: a -> Int
 
 instance AudioSample Double where
@@ -71,10 +135,16 @@ instance AudioSample (Double, Double) where
     collapse (a, b) = [a, b]
     numChans _ = 2
 
-class AudioSample a => MultiChannel a where
+-- | Type class for multi-channel audio samples
+class (AudioSample a) => MultiChannel a where
+    -- | Apply function to all channels
     mapChannels :: (Double -> Double) -> a -> a
+
+    -- | Get list of channel values
     channels :: a -> [Double]
     channels = collapse
+
+    -- | Create from list of channel values
     fromChannels :: [Double] -> Maybe a
 
 instance MultiChannel Double where
@@ -87,9 +157,13 @@ instance MultiChannel (Double, Double) where
     fromChannels [l, r] = Just (l, r)
     fromChannels _ = Nothing
 
-class MultiChannel a => AudioChannel a where
+-- | Type class for audio channels that support gain and panning
+class (MultiChannel a) => AudioChannel a where
+    -- | Apply gain to all channels
     gain :: Double -> a -> Either AudioError a
     gain g = Right . mapChannels (* g)
+
+    -- | Apply panning (-1 to 1, where -1 is left, 0 is center, 1 is right)
     pan :: Double -> a -> Either AudioError a
 
 instance AudioChannel Double where
@@ -98,8 +172,11 @@ instance AudioChannel Double where
 instance AudioChannel (Double, Double) where
     pan p x
         | p < -1 || p > 1 = Left $ InvalidPan p
-        | otherwise = Right $ fromMaybe (error "Impossible: stereo pan conversion failed") $
-            fromChannels $ doPan (channels x)
+        | otherwise =
+            Right $
+                fromMaybe (error "Impossible: stereo pan conversion failed") $
+                    fromChannels $
+                        doPan (channels x)
       where
         doPan [l, r] =
             let leftGain = cos ((p + 1) * pi / 4) -- Equal power panning
@@ -107,5 +184,8 @@ instance AudioChannel (Double, Double) where
              in [l * leftGain, r * rightGain]
         doPan _ = error "Impossible: stereo channels /= 2"
 
+-- | Monophonic signal type
 type Mono p = Signal p () Double
+
+-- | Stereophonic signal type
 type Stereo p = Signal p () (Double, Double)
